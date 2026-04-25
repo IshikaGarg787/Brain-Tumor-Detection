8f7c86cc79b77ca6b8fd5527373c843fd58cc6d7
Ishika Garg <ishikagarg330@gmail.com>
2026-04-25 12:23:05 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+app = Flask(__name__)
+
+MODEL_PATH = "brain_tumor_model1.h5"
+model = load_model(MODEL_PATH)
+
+IMG_HEIGHT = 256
+IMG_WIDTH = 256
+
+labels = ["glioma", "meningioma", "notumor", "pituitary"]
+
+def prepare_image(path):
+    img = image.load_img(path, target_size=(IMG_HEIGHT, IMG_WIDTH))
+    img_array = image.img_to_array(img)
+
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    return img_array
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        file = request.files["file"]
+
+        upload_folder = os.path.join("static", "uploads")
+        os.makedirs(upload_folder, exist_ok=True)
+
+        filepath = os.path.join(upload_folder, file.filename)
+        file.save(filepath)
+        
+
+        img = prepare_image(filepath)
+
+        preds = model.predict(img)
+
+        predicted_class = labels[np.argmax(preds)]
+        confidence = float(np.max(preds))
+
+        return render_template(
+            "index.html",
+            prediction=predicted_class,
+            confidence=f"{confidence*100:.2f}",
+            img_path = filepath.replace("\\", "/")
+        )
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
